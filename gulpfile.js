2f5109ffb070d64207d3535671726a2ef67d61e0
fortune ikechukwu ekeruo <legalmody@gmail.com>
2017-01-24 12:48:47 +0100

    Add a local deploy gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 
+const LOCAL_OWA_FOLDER = '../openmrs/openmrs-server/owa/';
 const THIS_APP_NAME = 'openWebApp';
 
 const sources = ['manifest.webapp', '**/*.html', '**/*.js', '**/*.css', '**/*.png',
@@ -11,4 +12,9 @@ gulp.task('default', () => {
   return gulp.src(sources)
     .pipe(zip(THIS_APP_NAME + '.zip'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy-local', () => {
+  return gulp.src(sources)
+    .pipe(gulp.dest(LOCAL_OWA_FOLDER + THIS_APP_NAME));
 });
